28cc50d5957ce1fdcebde25ae7bacc4872eb0b59
Marcus <m.h.otterstreom@gmail.com>
2026-09-15 11:54:22 +0000

    Remove unused functions related to trimming empty edge chart points

--- src/lib/chart-utils.ts
+++ src/lib/chart-utils.ts
@@ -22,37 +22,6 @@ interface DynamicCategoryTickLayout {
   height: number;
 }
 
-// function hasValueAtPoint<T extends Record<string, ChartPointValue>>(
-//   point: T,
-//   seriesNames: string[],
-// ) {
-//   return seriesNames.some((seriesName) => {
-//     const value = point[seriesName];
-//     return typeof value === "number" && Number.isFinite(value) && value !== 0;
-//   });
-// }
-
-// export function trimEmptyEdgeChartPoints<T extends Record<string, ChartPointValue>>(
-//   points: T[],
-//   seriesNames: string[],
-// ) {
-//   const firstNonEmptyIndex = points.findIndex((point) => hasValueAtPoint(point, seriesNames));
-//   if (firstNonEmptyIndex === -1) {
-//     return points;
-//   }
-
-//   let lastNonEmptyIndex = points.length - 1;
-//   while (lastNonEmptyIndex > firstNonEmptyIndex) {
-//     if (hasValueAtPoint(points[lastNonEmptyIndex], seriesNames)) {
-//       break;
-//     }
-
-//     lastNonEmptyIndex -= 1;
-//   }
-
-//   return points.slice(firstNonEmptyIndex, lastNonEmptyIndex + 1);
-// }
-
 export function getDynamicCategoryTickLayout({
   chartWidthPx,
   pointCount,
